6a4d927aae2a7afd35544dd8b657a54b192f5545
Kevin Tran <realxkvin@gmail.com>
2026-02-01 17:30:30 -0800

    Initial java program

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,8 @@
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        double celsius = 25;
+        double fahrenheit = celsius * 2;
+        System.out.println(celsius + " degrees Celsius is " + fahrenheit + " degrees Fahrenheit.");
+    }
+}
