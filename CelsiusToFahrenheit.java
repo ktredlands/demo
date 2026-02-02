91eccae67e515be7dd229587e9bdb6a202b87779
Kevin Tran <realxkvin@gmail.com>
2026-02-01 17:33:45 -0800

    corrected c to f logic

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -2,7 +2,7 @@
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
         double celsius = 25;
-        double fahrenheit = celsius * 2;
+        double fahrenheit = (celsius * 9 / 5) + 32;
         System.out.println(celsius + " degrees Celsius is " + fahrenheit + " degrees Fahrenheit.");
     }
 }
